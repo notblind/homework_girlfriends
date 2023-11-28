24c2d3f1d74e46e9538701de6ec4c0226cd89310
dbadvisability <121114112+dbadvisability@users.noreply.github.com>
2023-11-28 02:05:05 +0100

    added description for fields

--- database/models.py
+++ database/models.py
@@ -6,9 +6,9 @@ from database.settings import db_handle
 class Group(Model):
     id = PrimaryKeyField(null=False)
 
-    resource_id = CharField(max_length=100)
-    name = CharField(max_length=100)
-    screen_name = CharField(max_length=100)
+    resource_id = CharField(max_length=100)  # Id группы в вк
+    name = CharField(max_length=100)  # Наименование группы
+    screen_name = CharField(max_length=100)  # Краткое имя группы
 
     class Meta:
         database = db_handle
@@ -18,17 +18,17 @@ class Group(Model):
 class Post(Model):
     id = PrimaryKeyField(null=False)
 
-    group = ForeignKeyField(Group, to_field="id")
-    resource_id = CharField(max_length=100)
-    likes = IntegerField()
-    reactions = IntegerField()
-    views = IntegerField()
-    comments = IntegerField()
-    reposts = IntegerField()
-    text = CharField(max_length=100)
+    group = ForeignKeyField(Group, to_field="id")  # Ссылка на таблицу группу, к которой относится пост
+    resource_id = CharField(max_length=100)  # Id поста в вк
+    likes = IntegerField()  # Кол-во лайков
+    reactions = IntegerField()  # Кол-во реакций
+    views = IntegerField()  # Кол-во просмотров
+    comments = IntegerField()  # Кол-во комментов
+    reposts = IntegerField()  # Кол-во репостов
+    text = CharField(max_length=100)  # Текст поста
 
-    has_photo = BooleanField(default=False)
-    has_video = BooleanField(default=False)
+    has_photo = BooleanField(default=False)  # У поста есть фото
+    has_video = BooleanField(default=False)  # У поста есть видео
 
     class Meta:
         database = db_handle
